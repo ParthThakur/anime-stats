d14ada7bfea95cf77784f024f6959cae132bd909
ParthThakur <parththakur@gmail.com>
2021-04-27 15:22:01 +0100

    Save anime list locally

--- anime_dashboard/backend/views.py
+++ anime_dashboard/backend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 
 from rest_framework import generics, status
 from rest_framework.views import APIView
@@ -9,21 +10,34 @@ import itertools
 import requests
 import json
 import asyncio
+from pathlib import Path
 
 
-def get_user_list(username):
-    ani_list = []
-    url = f'https://myanimelist.net/animelist/{username}/load.json'
-    for offset in itertools.count(step=300):
-        data = requests.get(url + f'?offset={offset}')
-        ani_list.extend(data.json())
-        if len(data.json()) < 300:
-            break
+def get_user_list(username, refresh=False):
+    if refresh:
+        ani_list = []
+        url = f'https://myanimelist.net/animelist/{username}/load.json'
+        for offset in itertools.count(step=300):
+            data = requests.get(url + f'?offset={offset}')
+            ani_list.extend(data.json())
+            if len(data.json()) < 300:
+                break
 
-        if offset % 300 > 20:
-            break
+            if offset % 300 > 20:
+                break
 
-    return ani_list
+        with open(f'data/anime_lists/{username}.json', 'w') as fw:
+            json.dump({'anime_list': ani_list,
+                      'last_updated': timezone.now().strftime("%m/%d/%Y, %H:%M:%S")}, fw)
+        return ani_list
+
+    else:
+        try:
+            with open(f'data/anime_lists/{username}.json', 'r') as fr:
+                data = json.load(fr)
+                return data['anime_list']
+        except FileNotFoundError:
+            return get_user_list(username, refresh=True)
 
 
 class UserList(APIView):
