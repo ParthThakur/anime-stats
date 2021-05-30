be597501757c4c8a76415b743e15a3615dd01b9c
ParthThakur <parththakur@gmail.com>
2021-05-30 22:35:23 +0100

    Create homepage

--- anime_dashboard/frontend/src/components/app.js
+++ anime_dashboard/frontend/src/components/app.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Homepage from "./homepage";
 
 export default function App(props) {
   return (
-    <div>
-      <h1>The Anime Dashboard!</h1>
+    <div className="app-inner-container">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
