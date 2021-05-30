4cd82f1e71cb59d2ba9b22e292e8b8118589798a
ParthThakur <parththakur@gmail.com>
2021-05-30 18:42:15 +0100

    set up webpack

---
+++ anime_dashboard/frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name]-[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: NODE_ENV === "production",
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+};
